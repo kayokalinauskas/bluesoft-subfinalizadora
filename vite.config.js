37f29835fa974ba0f9e0f2d670fc8eb79d963aa2
kayokalinauskas <kayokalinauskas@users.noreply.github.com>
2025-10-29 14:57:01 +0000

    deploy: f31fbf24b8712a3ff38534f29a4ff077dc188882

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  base: "/bluesoft-subfinalizadora/", // 👈 coloque o nome do repositório aqui
-  plugins: [tailwindcss()],
-});
